ca18c1a624dcd207a95b0a571665bc56c86e69d9
Hsiaoming Yang <me@lepture.com>
2026-06-18 02:42:30 +0900

    docs: add logo and custom css

--- docs/conf.py
+++ docs/conf.py
@@ -14,3 +14,16 @@ source_suffix = {
 html_theme = 'shibuya'
 html_title = 'Wenmode'
 html_baseurl = 'https://wenmode.lepture.com/'
+html_static_path = ['_static']
+html_logo = '_static/light-logo.svg'
+html_favicon = '_static/wenmode-mark.svg'
+html_css_files = [
+    'custom.css',
+]
+
+html_theme_options = {
+    'discussion_url': 'https://github.com/lepture/wenmode/discussions',
+    'accent_color': 'red',
+    'light_logo': '_static/light-logo.svg',
+    'dark_logo': '_static/dark-logo.svg',
+}
