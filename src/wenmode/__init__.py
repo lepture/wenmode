f574fe6dc37c4514735cda7ac91888351926b30c
Hsiaoming Yang <me@lepture.com>
2026-06-18 18:59:51 +0900

    chore: release 0.1.0

--- src/wenmode/__init__.py
+++ src/wenmode/__init__.py
@@ -2,6 +2,11 @@ from .parser import Parser, StreamingUnsupportedError
 from .renderers import HTMLRenderer, MarkdownRenderer, RSTRenderer
 from .wenmode import Wenmode
 
+__version__ = '0.1.0'
+__homepage__ = 'https://wenmode.lepture.com/'
+__author__ = 'Hsiaoming Yang <me@lepture.com>'
+__license__ = 'BSD-3-Clause'
+
 __all__ = [
     'HTMLRenderer',
     'MarkdownRenderer',
