dfc9a47a96f1d6af0acb597ceeb9344cc9c477d1
Hsiaoming Yang <me@lepture.com>
2026-07-13 15:04:12 +0900

    feat: export BlockFenced InlineDelimited and InlineLiteral

--- src/wenmode/plugins/__init__.py
+++ src/wenmode/plugins/__init__.py
@@ -1,6 +1,9 @@
 """Built-in Wenmode plugins and plugin API types."""
 
+from .._declaratives import BlockFenced as BlockFenced
+from .._declaratives import InlineDelimited as InlineDelimited
+from .._declaratives import InlineLiteral as InlineLiteral
 from .types import PluginModule as PluginModule
 from .types import RendererHandlers as RendererHandlers
 
-__all__ = ['PluginModule', 'RendererHandlers']
+__all__ = ['BlockFenced', 'InlineDelimited', 'InlineLiteral', 'PluginModule', 'RendererHandlers']
